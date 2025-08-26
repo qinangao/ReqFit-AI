d83e5da4ccab3a901113df8fb051b445f4c24302
qinanagao@gmail.com <qinanagao@gmail.com>
2025-08-26 17:37:33 +0100

    add home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import NavBar from "~/components/NavBar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ReqFit AI" },
+    {
+      name: "description",
+      content:
+        "Making hiring smarter by aligning skills with roles that truly fit.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <NavBar />
+      <section className="main-section">
+        <div className="page-heading py-8 md:py-16">
+          <h1>Track your applications & Resume Rating</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <section className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </section>
+        )}
+      </section>
+    </main>
+  );
 }
