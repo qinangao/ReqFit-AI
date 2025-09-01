94e67f31f9178437793c057917ba69ea5e30d9e8
qinanagao@gmail.com <qinanagao@gmail.com>
2025-09-01 18:05:28 +0100

    add cn function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,6 @@
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function formatSize(bytes: number): string {
   if (bytes === 0) return "0 Bytes";
 
@@ -10,3 +13,7 @@ export function formatSize(bytes: number): string {
 }
 
 export const generateUUID = () => crypto.randomUUID();
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
